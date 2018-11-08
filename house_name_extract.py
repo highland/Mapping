b5446d51fe44750789494ae239949e630a9d350c
Mark Thomas <mark.highland@gmail.com>
2018-11-08 19:31:51 +0000

    Updates to prepare for the use of Neo4j

--- house_name_extract.py
+++ house_name_extract.py
@@ -5,33 +5,60 @@ Author: Mark Thomas
 """
 from pprint import pprint
 
-from typing import Sequence, List, Tuple
+from typing import Sequence, List, Tuple, Dict
 
 import requests
 from bs4 import BeautifulSoup
+from OSGridConverter import latlong2grid
+from shapely.geometry.polygon import Polygon
 
 # Type aliases
 XML = str
 Tags = str
 Keys = List[Tags]
-House_info = List[Tuple[str]]
+HouseInfo = List[Tuple[str]]
+Eastings = int
+Northings = int
+Coords = Tuple[Eastings, Northings]
 
-soup: BeautifulSoup = None
 
 def _get_raw_data() -> XML:
-    newtonmore = {'bbox': '-4.1386,57.0572,-4.0877,57.0729'}
+    newtonmore = {'bbox': '-4.1516,57.0421,-4.0918,57.0772'}
     osm = 'https://api.openstreetmap.org/api/0.6/map'
     return requests.get(osm, newtonmore).text
 
 
+soup: BeautifulSoup = BeautifulSoup(_get_raw_data(), 'xml')
+
+all_corners: Dict[str, Coords] = {}
+
+
+def _get_corners():
+    """
+    Populate the Global Dictionary, all_corners, with the coordinates
+    of all the nodes in the data set.
+    """
+    if not all_corners:
+        for tag in soup.find_all('node'):
+            ref = tag.get('id')
+            lat = float(tag.get('lat'))
+            lon = float(tag.get('lon'))
+            grid = latlong2grid(lat, lon)
+            all_corners[ref] = grid.E, grid.N
+
+
 def _clean(seq: Sequence) -> List:
+    """
+    Remove duplicates and sort
+    """
     alist = list(set(seq))
     alist.sort()
     return alist
 
 
-def extract_house_info(keys: Keys) -> House_info:
-    """Get House names plus additional data.
+def extract_house_info(keys: Keys) -> HouseInfo:
+    """
+    Get House names plus additional data.
 
     Args:
         keys: The required extra tags (k = ... in OSM XML)
@@ -39,11 +66,8 @@ def extract_house_info(keys: Keys) -> House_info:
     Returns:
         The requested data
     """
-    global soup
-    if not soup:
-        data = _get_raw_data()
-        soup = BeautifulSoup(data, 'xml')
-    house_list = []
+    _get_corners()
+    house_list: HouseInfo = []
     for tag in soup.find_all('tag'):
         if tag.get('k') == 'addr:housename':
             info = []
@@ -53,9 +77,21 @@ def extract_house_info(keys: Keys) -> House_info:
                 key = data.get('k')
                 if key in keys:
                     info.append(data.get('v'))
+            corners: List[Coords] = []
+            for place in house.find_all('nd'):
+                ref = place.get('ref')
+                corners.append(all_corners.get(ref))
+            if len(corners) >= 3:
+                point = Polygon(corners).centroid
+                info.append(int(point.x))
+                info.append(int(point.y))
             house_list.append(tuple(info))
     return _clean(house_list)
 
+
+# def meters_distance(house1: Point, house2: Point) -> int:
+#    return int(sqrt((house1.E - house2.E)**2 + (house1.N - house2.N)**2))
+# now just 'house1.distance(house2)'
 
 def get_house_names() -> List[str]:
     """Get house names.
@@ -63,28 +99,20 @@ def get_house_names() -> List[str]:
     Returns:
         All the house names in the bounding block.
     """
-    global soup
-    if not soup:
-        data = _get_raw_data()
-        soup = BeautifulSoup(data, 'xml')
     return _clean([tag.get('v')
                    for tag in soup.find_all('tag')
                    if tag.get('k') == 'addr:housename'])
 
 
-def get_previous_names() -> List[Tuple[str,str]]:
+def get_previous_names() -> List[Tuple[str, str]]:
     """Get old house names.
 
     Returns:
         Tuples of (new name, old name).
     """
-    global soup
-    if not soup:
-        data = _get_raw_data()
-        soup = BeautifulSoup(data, 'xml')
     replacement_list = []
     for tag in soup.find_all('tag'):
-        if tag.get('k') == 'addr:previousname':
+        if tag.get('k') == 'old_name':
             old_name = tag.get('v')
             house = tag.parent
             data = house.tag
@@ -102,13 +130,30 @@ def _main():
     pprint(get_house_names()[:10])
 
     houses = extract_house_info(['addr:street', 'addr:postcode'])
-    print('Name, Street, Postcode for first 10 houses:')
+    print('Name, Postcode, Street, Eastings, Northings for first 10 houses:')
     pprint(houses[:10])
-    
-    print(f'{len(houses)} houses found' )
+    count = 0
+    with open('houses.csv', 'w') as outfile:
+        outfile.write('Name,Postcode,Road,OSE,OSN\n')   # csv header
+        for house in houses:
+            if len(house) == 5:
+                name, postcode, road, os_eastings, os_northings = house
+                outfile.write(
+                    f'{name},{postcode},{road},{os_eastings},{os_northings}\n')
+                count += 1
+    print(f'{count} houses found')
 
     print('House names that have changed:')
     pprint(get_previous_names())
+
+
+def _main1():
+    from collections import Counter
+    tags = Counter()
+    for tag in soup.find_all('tag'):
+        tags[tag.get('k')] += 1
+    pprint(tags.most_common())
+
 
 if __name__ == '__main__':
     _main()
